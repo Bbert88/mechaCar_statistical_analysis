1a3316cd4d4e75aa4e54be04ac5d7f195317f885
Brett Bertoni <bbertoni1@gmail.com>
2022-07-20 21:29:54 -0400

    add additional analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,8 +6,25 @@ car_df <- data.frame(car_data)
 
 head(car_df)
 
-#Perform Linear Regression
+#Perform Linear Regression to predict MPG
+
 lr <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance +
      AWD, data=car_df)
 
 summary(lr)
+
+#Summary Statistics on Suspension Coils
+
+coil_data <- read.csv("Suspension_Coil.csv")
+head(coil_data)
+
+# Summary stats for PSI column
+
+total_summary <- coil_data %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+print(total_summary)
+
+# Group by Lot
+
+lot_summary <- coil_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+print(lot_summary)
+
